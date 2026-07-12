d365d1b88df4f3be57025fa5557ed7dcd744577b
dani811 <jusdadodaniel@gmail.com>
2026-07-12 15:40:24 +0200

    test(backend): verify application context

---
+++ backend/src/test/java/com/dani811/arhome/ArHomeApplicationTest.java
@@ -0,0 +1,12 @@
+package com.dani811.arhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArHomeApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
